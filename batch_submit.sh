07033af314ba5571e58c770a3bafbe6ed9db6da2
Ryen Krusinga <krusinga@cs.umd.edu>
2020-09-02 12:35:47 -0400

    update batch_submit

--- batch_submit.sh
+++ batch_submit.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos=high
 #SBATCH --mem=32gb
 ###SBATCH -c 10  ### Use this to ask for a certain number of cores? (cpuspertask)
-###SBATCH -n 10  ### number of multiprocessing tasks? (ntasks)
+#SBATCH -n 6  ### number of multiprocessing tasks? (ntasks) 
 ###SBATCH --account scavenger
 ###SBATCH --partition scavenger
 #SBATCH --gres=gpu:4 ### gpu:p6000:1
@@ -20,8 +20,10 @@
 echo "Hello from: "
 hostname
 
+
+# 4 data workers is optimal
 cd /cfarhomes/krusinga/storage/research/causality/granger-causal-frames
-time python -u decord_traintest.py --batch_size 128 --num_data_workers 8 --n_epochs 2
+time python -u decord_traintest.py --batch_size 128 --num_data_workers 4 --n_epochs 2
 #module load cuda
 #python -u traintest.py --line ${SLURM_ARRAY_TASK_ID}
 #python run.py --stage 2 --id 72 --masterconfig /fs/vulcan-scratch/krusinga/projects/ganProbability/master.yaml  --pid ${SLURM_ARRAY_TASK_ID} --nprocs ${SLURM_ARRAY_TASK_COUNT}
