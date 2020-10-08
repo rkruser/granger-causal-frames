f86d649a0d3778583b03db7ccaee0947df3780f7
Ryen Krusinga <rkrusinga@gmail.com>
2020-10-08 10:50:20 -0400

    FeatureSequenceRenderer seems to work

--- general_interface/artificial_data.py
+++ general_interface/artificial_data.py
@@ -34,6 +34,31 @@ terminal_1 = np.abs(transition_matrix_1.diagonal()-1) < 1e-8
 rewards_1 = np.array([0.0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
 
+
+
+
+null_object_1 = np.array([0.0, 0, 0])
+choice_matrix_1 = [[-1,-1],[-1,1],[1,-1],[1,1]]
+
+
+def choice_function_1(choice_matrix, terminal_reward):
+    choice = np.random.choice(4)
+    feature_label = 1.0 if choice in [0,3] else 0.0 #Not linearly separable
+    feature_choice = choice_matrix[choice]
+ 
+    return feature_choice, feature_label
+
+def choice_function_2(choice_matrix, terminal_reward):
+    if terminal_reward == 1:
+        choice = np.random.choice(4, p=[0.5, 0.0, 0.0, 0.5])
+    else:
+        choice = np.random.choice(4, p=[0.0, 0.5, 0.5, 0.0])
+    feature_label = 1.0 if choice in [0,3] else 0.0 #Not linearly separable
+    feature_choice = choice_matrix[choice]
+
+    return feature_choice, feature_label
+
+
 '''
 Rendering functions:
     For use in artificial sequence generators
@@ -50,62 +75,83 @@ feature_label : The constant feature label
 '''
 
 
-null_object_1 = np.array([0.0, 0, 0])
 
-def render_sequence_1(states, terminal_label, state_mapping=state_mapping_1):
-    sequence_values = []
-
-    choice_matrix = [[-1,-1],[-1,1],[1,-1],[1,1]] #Can make this correlate with terminal_label instead
-    choice = np.random.choice(4)
-    feature_label = 1.0 if choice in [0,3] else 0.0 #Not linearly separable
-    features = choice_matrix[choice]
-
-    for s in states:
-        sequence_values.append([state_mapping[s]]+features)
-
-    feature_labels = np.empty(len(states))
-    feature_labels.fill(feature_label)
-
-    return np.array(sequence_values), feature_labels, feature_label
-
-
-'''
-Like render_sequence_1, except feature_label is highly correlated with terminal label
-'''
-def render_sequence_2(states, terminal_label, state_mapping=state_mapping_1):
-    sequence_values = []
-
-    choice_matrix = [[-1,-1],[-1,1],[1,-1],[1,1]]
-    if terminal_label == 1:
-        choice = np.random.choice(4, p=[0.4, 0.1, 0.1, 0.4])
-    else:
-        choice = np.random.choice(4, p=[0.1, 0.4, 0.4, 0.1])
-    feature_label = 1.0 if choice in [0,3] else 0.0 #Not linearly separable
-    features = choice_matrix[choice]
-
-    for s in states:
-        sequence_values.append([state_mapping[s]]+features)
-
-    feature_labels = np.empty(len(states))
-    feature_labels.fill(feature_label)
-
-    return np.array(sequence_values), feature_labels, feature_label
+#def render_sequence_1(states, terminal_label, state_mapping=state_mapping_1):
+#    sequence_values = []
+#
+#    choice_matrix = [[-1,-1],[-1,1],[1,-1],[1,1]] #Can make this correlate with terminal_label instead
+#    choice = np.random.choice(4)
+#    feature_label = 1.0 if choice in [0,3] else 0.0 #Not linearly separable
+#    features = choice_matrix[choice]
+#
+#    for s in states:
+#        sequence_values.append([state_mapping[s]]+features)
+#
+#    feature_labels = np.empty(len(states))
+#    feature_labels.fill(feature_label)
+#
+#    return np.array(sequence_values), feature_labels, feature_label
+#
+#
+#'''
+#Like render_sequence_1, except feature_label is highly correlated with terminal label
+#'''
+#def render_sequence_2(states, terminal_label, state_mapping=state_mapping_1):
+#    sequence_values = []
+#
+#    choice_matrix = [[-1,-1],[-1,1],[1,-1],[1,1]]
+#    if terminal_label == 1:
+#        choice = np.random.choice(4, p=[0.4, 0.1, 0.1, 0.4])
+#    else:
+#        choice = np.random.choice(4, p=[0.1, 0.4, 0.4, 0.1])
+#    feature_label = 1.0 if choice in [0,3] else 0.0 #Not linearly separable
+#    features = choice_matrix[choice]
+#
+#    for s in states:
+#        sequence_values.append([state_mapping[s]]+features)
+#
+#    feature_labels = np.empty(len(states))
+#    feature_labels.fill(feature_label)
+#
+#    return np.array(sequence_values), feature_labels, feature_label
 
 
 
 '''
 Take a state sequence and a terminal label and return a sequence of rewards at each state.
 '''
-def label_sequence_1(states, terminal_label):
+def label_sequence_1(states, terminal_reward):
     rewards = np.zeros(len(states))
+    terminal_label = terminal_reward
     rewards[-1] = terminal_label
-    return rewards
+    return rewards, terminal_label
+
+def label_sequence_2(states, terminal_reward):
+    rewards = np.zeros(len(states))
+    terminal_label = np.random.choice([0.0, 1.0])
+    rewards[-1] = terminal_label
+    return rewards, terminal_label
 
 
+'''
+Assume reward based only on terminal markov state
+'''
+def value_extractor_1(analytical_values, pure_values, state, feature_state):
+    base_value = analytical_values[state]
+    return base_value
 
-def value_extractor_1(analytical_values, state, feature_state):
-    pass
+'''
+Assume reward based only on feature state
+'''
+def value_extractor_2(analytical_values, pure_values, state, feature_state):
+    if feature_state in [0,3]:
+        return pure_values[state]
+    else:
+        return 0.0
 
+
+# What is the state value if the reward is based only on the features?
+# I guess it is final_reward * discount^(expected #steps to any terminal state)
 
 
 '''
@@ -132,7 +178,11 @@ class SequenceRenderer:
 Render sequences by performing the state mapping, then adding constant features from the choice matrix
 '''
 class FeatureSequenceRenderer(SequenceRenderer):
-    def __init__(self, state_mapping, choice_matrix, choice_function, label_function, value_function):
+    def __init__(self, state_mapping=state_mapping_1, 
+                       choice_matrix=choice_matrix_1, 
+                       choice_function=choice_function_1, 
+                       label_function=label_sequence_1, 
+                       value_function=value_extractor_1):
         self.state_mapping = state_mapping
         self.choice_matrix = choice_matrix
         self.choice_function = choice_function
@@ -144,9 +194,9 @@ class FeatureSequenceRenderer(SequenceRenderer):
         mapped_values = []
 
         sequence_labels, global_label = self.label_function(state_sequence, terminal_reward)
-        feature_choice, global_feature_label = self.choice_function(self.choice_matrix, terminal_reward)
+        feature_choice, global_feature_label = self.choice_function(self.choice_matrix, global_label) #note the dependence on global label
 
-        for s in states:
+        for s in state_sequence:
             mapped_values.append(np.concatenate((self.state_mapping[s], feature_choice)))
 
         mapped_values = np.array(mapped_values)
@@ -164,16 +214,17 @@ class FeatureSequenceRenderer(SequenceRenderer):
         for i, c in enumerate(self.choice_matrix):
             for j,s in enumerate(self.state_mapping):
                 combos.append(np.concatenate((s,c)))
-                state_pairs.append( (i, j) )
+                state_pairs.append( (j, i) )
         combos = np.array(combos)
         return combos, state_pairs
 
     
-    def get_value(self, analytical_values, state, feature_state):
-        return self.value_function(analytical_values, state, feature_state)
+    def get_value(self, analytical_values, pure_values, state, feature_state):
+        return self.value_function(analytical_values, pure_values, state, feature_state)
 
 
-
+feature_renderer_1 = FeatureSequenceRenderer(state_mapping_1, choice_matrix_1, choice_function_1, label_sequence_1, value_extractor_1)
+feature_renderer_2 = FeatureSequenceRenderer(state_mapping_1, choice_matrix_1, choice_function_2, label_sequence_1, value_extractor_2)
 
 '''
 MarkovProcess
@@ -191,9 +242,7 @@ class MarkovProcess:
     def __init__(self, transition_matrix=transition_matrix_1, 
                        terminal = terminal_1,
                        rewards = rewards_1,
-                       sequence_render = render_sequence_1,
-                       sequence_labeler = label_sequence_1,
-                       random_terminal=False,
+                       renderer = feature_renderer_1
                        ):
 
         self.transition_matrix = transition_matrix
@@ -201,12 +250,22 @@ class MarkovProcess:
         self.terminal_states = np.arange(len(transition_matrix),dtype=int)[terminal]
         self.rewards = rewards
         self.terminal_rewards = rewards[terminal]
-        self.sequence_renderer = sequence_render
-        self.sequence_labeler = sequence_labeler
-        self.random_terminal = random_terminal
+        self.renderer = renderer
 
         self.n_states = len(self.transition_matrix)
         self.state_indices = np.arange(self.n_states, dtype=int)
+
+
+    def get_all_states_and_values(self, discount):
+        combos, state_pairs = self.renderer.get_all_combinations()
+        analytical_vals = self.analytical_values(discount)
+        pure_vals = self.pure_discounted_values(discount)
+
+        values = []
+        for p in state_pairs:
+            v = self.renderer.get_value(analytical_vals, pure_vals, p[0], p[1])
+            values.append(v)
+        return combos, np.array(values)
 
 
     '''
@@ -218,12 +277,11 @@ class MarkovProcess:
       states : The abstract markov states corresponding to the sequence (may have different length than the sequence, as the sequence may be much expanded).
     '''
     def sample(self):
-        states, terminal_label = self.sample_states()
-        rewards = self.sequence_labeler(states, terminal_label)
+        states, terminal_reward = self.sample_states()
+        mapped_states, rewards, feature_labels, global_label, global_feature_label = self.renderer.render(states, terminal_reward)
+        global_labels = (global_label, global_feature_label)
 
-        sequence, feature_labels, global_feature_label = self.sequence_renderer(states, terminal_label)
-        global_labels = (terminal_label, global_feature_label)
-        return sequence, rewards, feature_labels, global_labels, states
+        return mapped_states, rewards, feature_labels, global_labels, states
 
 
     '''
@@ -237,26 +295,48 @@ class MarkovProcess:
             state = np.random.choice(self.n_states, p=self.transition_matrix[state])
             state_sequence.append(state)
 
-        if self.random_terminal:
-            terminal_label = np.random.choice([0.0,1.0])
-        else:
-            terminal_label = self.rewards[state_sequence[-1]]
 
-        return np.array(state_sequence), terminal_label
+        terminal_reward = self.rewards[state_sequence[-1]]
+
+        return np.array(state_sequence), terminal_reward
 
 
     '''
     Get the analytical values of the markov chain for a given discount factor
     '''
     def analytical_values(self, discount):
+        return self.expected_discounted_rewards(discount, self.terminal_rewards)
+
+    '''
+    Get the expected rewards assuming all terminal states yield reward 1
+    '''
+    def pure_discounted_values(self, discount):
+        return self.expected_discounted_rewards(discount, np.ones(len(self.terminal_rewards)))
+
+    '''
+    This function is separate so you can choose your own terminal rewards, e.g. for different feature scenarios
+    '''
+    def expected_discounted_rewards(self, discount, terminal_rewards):
         m_hat = self.transition_matrix[~self.terminal]
-        v_hat = np.dot(m_hat[:,self.terminal], self.terminal_rewards)
+        v_hat = np.dot(m_hat[:,self.terminal], terminal_rewards)
         m_hat = m_hat[:,~self.terminal]
-        x_hat = np.linalg.solve(m_hat-(1.0/discount)*np.eye(len(m_hat)), -v_hat)
+        x_hat = np.linalg.solve((1.0/discount)*np.eye(len(m_hat))-m_hat, v_hat)
         x = np.empty(len(self.transition_matrix))
         x[~self.terminal] = x_hat
-        x[self.terminal] = self.terminal_rewards
+        x[self.terminal] = terminal_rewards
         return x
+
+    def expected_steps_to_terminal(self):
+        m_hat = self.transition_matrix[~self.terminal, ~self.terminal]
+        v_hat = np.ones(len(m_hat))
+        x_hat = np.linalg.solve(np.eye(len(m_hat))-m_hat, v_hat)
+        expected = np.empty(len(self.transition_matrix))
+        expected[~self.terminal] = x_hat
+        expected[self.terminal] = 0.0
+        return expected
+
+    # Next: make use of the value function to get values for all states
+    # Value function should take analytical values and expected steps to terminal
 
 
 
@@ -318,11 +398,27 @@ def test2():
     print("0.5", mp.analytical_values(0.5))
 
  
+def test3():
+    mp = MarkovProcess()
+    print(mp.expected_discounted_rewards(1.0, np.array([0.0, 1.0])))
+
+def test4():
+    mp1 = MarkovProcess(renderer=feature_renderer_1)
+    mp2 = MarkovProcess(renderer=feature_renderer_2)
+
+    combos1, vals1 = mp1.get_all_states_and_values(0.977)
+
+    combos2, vals2 = mp2.get_all_states_and_values(0.977)
+
+    print("MP1:", combos1, vals1)
+    print("MP2:", combos2, vals2)
 
 
 if __name__ == '__main__':
 #    test1()
-    test2()
+#    test2()
+#    test3()
+    test4()
 
 
 
